97cf644fe176ee978a08e179495eeb4eb6d257fb
IvankoArtem <tema30k@gmail.com>
2023-06-29 10:48:42 +0300

    Updated config-overries imports

--- config-overrides.js
+++ config-overrides.js
@@ -3,6 +3,7 @@ const { alias } = require("react-app-rewire-alias");
 module.exports = function override(config) {
   alias({
     "@": "src",
+    "@shared": "src/components",
   })(config);
 
   return config;
