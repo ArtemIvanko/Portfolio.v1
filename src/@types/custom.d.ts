27164eb6ba7ff0ab364a568bc7d21fe40084b65d
IvankoArtem <tema30k@gmail.com>
2023-06-30 21:37:59 +0300

    Setted up custom.d.ts config

---
+++ src/@types/custom.d.ts
@@ -0,0 +1,21 @@
+type SvgComponent = React.FunctionComponent<React.SVGProps<SVGSVGElement>>;
+
+declare module "*.svg" {
+  const content: React.FunctionComponent<React.SVGAttributes<SVGElement>>;
+  export default content;
+}
+
+declare module "*.png" {
+  const pngSrc: string;
+  export default pngSrc;
+}
+
+declare module "*.jpg" {
+  const jpgSrc: string;
+  export default jpgSrc;
+}
+
+declare module "*.jpeg" {
+  const jpegSrc: string;
+  export default jpegSrc;
+}
