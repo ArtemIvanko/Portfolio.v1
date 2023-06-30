047b0c5282bc6f27f27eac10d28e333d464d01cd
IvankoArtem <tema30k@gmail.com>
2023-06-30 13:17:11 +0300

    Removed unecessary Footer bar styles

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -1,54 +1,37 @@
-import { IconButton, Typography, Link } from "@mui/material";
+import { Typography } from "@mui/material";
 import { ReactNode } from "react";
 import GitHubIcon from "@mui/icons-material/GitHub";
 import PinterestIcon from "@mui/icons-material/Pinterest";
 import TwitterIcon from "@mui/icons-material/Twitter";
 import TelegramIcon from "@mui/icons-material/Telegram";
+import { Link, LinkColor } from "@shared/utils/Link";
 import styled from "@/DefaultTheme";
 
 interface ISocialLinkProps {
   href: string;
-  icon: ReactNode;
+  icon?: ReactNode;
 }
 
 export const Footer = () => (
   <Root>
-    <CopyrightTitle variant="body2">
-      Copyright (c) 2023 Artem Ivanko
-    </CopyrightTitle>
-    <LinkContainer>
+    <Typography variant="body1">Copyright (c) 2023 Artem Ivanko</Typography>
+    <div>
       <SocialLink href="" icon={<GitHubIcon />} />
       <SocialLink href="" icon={<PinterestIcon />} />
       <SocialLink href="" icon={<TwitterIcon />} />
       <SocialLink href="" icon={<TelegramIcon />} />
-    </LinkContainer>
+    </div>
   </Root>
 );
 
 const Root = styled("div")(({ theme }) => ({
-  display: "flex",
-  flexDirection: "column",
-  width: "100%",
-  height: "5rem",
   background: theme.palette.primary.main,
   color: theme.palette.secondary.main,
   padding: "0.5rem",
 }));
 
-const CopyrightTitle = styled(Typography)(({ theme }) => ({
-  [theme.breakpoints.up("lg")]: {
-    fontSize: theme.typography.body1.fontSize,
-  },
-}));
-
-const LinkContainer = styled("div")({
-  display: "flex",
-});
-
 const SocialLink = ({ href, icon }: ISocialLinkProps) => (
-  <IconButton>
-    <Link color="secondary" href={href}>
-      {icon}
-    </Link>
-  </IconButton>
+  <Link href={href} color={LinkColor.Secondary}>
+    {icon}
+  </Link>
 );
