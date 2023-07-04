ff2dea323860c4d3485dec69546baba70049c494
IvankoArtem <tema30k@gmail.com>
2023-07-04 16:20:41 +0300

    Created and used common constants

---
+++ src/constants/common.ts
@@ -0,0 +1,25 @@
+export type IconVariant =
+  | "github"
+  | "pinterest"
+  | "twitter"
+  | "telegram"
+  | "css"
+  | "figma"
+  | "git"
+  | "html"
+  | "js"
+  | "node"
+  | "npm"
+  | "react"
+  | "yarn";
+
+export const skills: IconVariant[] = [
+  "html",
+  "css",
+  "js",
+  "react",
+  "git",
+  "yarn",
+  "npm",
+  "node",
+];
