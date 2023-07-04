ab41fa9c1ada8903745eced7d081a03252e0ac42
IvankoArtem <tema30k@gmail.com>
2023-07-04 13:51:41 +0300

    BrowserRouter -> HashRouter

--- src/pages/Router.tsx
+++ src/pages/Router.tsx
@@ -1,12 +1,12 @@
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { HashRouter, Navigate, Route, Routes } from "react-router-dom";
 import { Navigation } from "@shared/Navigation";
 import { Home } from "./Home";
 import { About } from "./About";
-import { Projects } from "./Pojects";
+import { Projects } from "./Projects";
 import { NotFound } from "./NotFound";
 
 export const Router = () => (
-  <BrowserRouter>
+  <HashRouter>
     <Routes>
       <Route path="/" element={<Navigation />}>
         <Route index element={<Navigate to="/home" />} />
@@ -16,5 +16,5 @@ export const Router = () => (
         <Route path="*" element={<NotFound />} />
       </Route>
     </Routes>
-  </BrowserRouter>
+  </HashRouter>
 );
