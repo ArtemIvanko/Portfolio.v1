1a07b2bca219c6e104dddd9689c145ec01f80a31
IvankoArtem <tema30k@gmail.com>
2023-07-05 16:49:48 +0300

    Created Router test

---
+++ src/pages/Router.test.tsx
@@ -0,0 +1,24 @@
+import { render } from "@testing-library/react";
+import { MemoryRouter } from "react-router-dom";
+import { Router } from "./Router";
+
+test("renders the correct components for each route", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/home"]}>
+      <Router />
+    </MemoryRouter>
+  );
+
+  expect(getByText("Home")).toBeInTheDocument();
+});
+
+test("renders NotFound component for unknown routes", () => {
+  const { getByText } = render(
+    <MemoryRouter initialEntries={["/unknown"]}>
+      <Router />
+    </MemoryRouter>
+  );
+
+  expect(getByText("Error 404")).toBeInTheDocument();
+  expect(getByText("Not Found.")).toBeInTheDocument();
+});
