d36296c3ae6a41b6b5e24e56993d844436bf09c1
IvankoArtem <tema30k@gmail.com>
2023-07-06 14:35:29 +0300

    Updated Router test

--- src/tests/Router.test.tsx
+++ src/tests/Router.test.tsx
@@ -1,24 +1,10 @@
-import { render } from "@testing-library/react";
-import { MemoryRouter } from "react-router-dom";
+import { render, screen } from "@testing-library/react";
 import { Router } from "../pages/Router";
 
 test("renders the correct components for each route", () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={["/home"]}>
-      <Router />
-    </MemoryRouter>
-  );
+  render(<Router />);
 
-  expect(getByText("Home")).toBeInTheDocument();
-});
-
-test("renders NotFound component for unknown routes", () => {
-  const { getByText } = render(
-    <MemoryRouter initialEntries={["/unknown"]}>
-      <Router />
-    </MemoryRouter>
-  );
-
-  expect(getByText("Error 404")).toBeInTheDocument();
-  expect(getByText("Not Found.")).toBeInTheDocument();
+  expect(screen.getByText("Home")).toBeInTheDocument();
+  expect(screen.getByText("About")).toBeInTheDocument();
+  expect(screen.getByText("Projects")).toBeInTheDocument();
 });
