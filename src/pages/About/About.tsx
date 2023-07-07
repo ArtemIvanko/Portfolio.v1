4d16ac85f941d897d21d7e20ce7581951664d1a0
IvankoArtem <tema30k@gmail.com>
2023-07-07 16:37:56 +0300

    Removed mocked data, used real instead

--- src/pages/About/About.tsx
+++ src/pages/About/About.tsx
@@ -1,36 +1,57 @@
 import { Icon } from "@utils/Icon";
 import { Typography } from "@mui/material";
-import { skills } from "@/constants/common";
+import { useState, useEffect } from "react";
+import { ISkillsProps } from "@/constants/common";
 import styled from "@/DefaultTheme";
 
-export const About = () => (
-  <Root>
-    <Typography variant="h5">Hi there!</Typography>
-    <Typography variant="body2">
-      I'm a passionate Frontend developer with expertise in JavaScript, HTML,
-      and CSS. Skilled in React, Git, and SASS, I have hands-on experience
-      creating web applications and designing user interfaces. I thrive in
-      collaborative environments, leveraging strong communication and teamwork
-      abilities to contribute effectively. Seeking opportunities to further
-      enhance my skills and make a valuable impact in a stable company committed
-      to innovation.
-    </Typography>
-    <Typography variant="h5">Skills:</Typography>
-    <IconContainer>
-      {skills.map(({ icon, title, description }) => (
-        <div key={title}>
-          {icon.map((variant) => (
-            <SkillTitle key={variant}>
-              <Icon icon={variant} />
-              <Typography variant="h5">{title}</Typography>
-            </SkillTitle>
-          ))}
-          <Typography>{description}</Typography>
-        </div>
-      ))}
-    </IconContainer>
-  </Root>
-);
+export const About = () => {
+  const [skillsData, setSkillsData] = useState([]);
+
+  useEffect(() => {
+    async function fetchSkillsData() {
+      try {
+        const response = await fetch(
+          "https://michaelwhite42.github.io/data/skills.json"
+        );
+        const { data } = await response.json();
+        setSkillsData(data);
+      } catch (error) {
+        console.error("Error fetching skills data:", error);
+      }
+    }
+
+    fetchSkillsData();
+  }, []);
+
+  return (
+    <Root>
+      <Typography variant="h5">Hi there!</Typography>
+      <Typography variant="body2">
+        I'm a passionate Frontend developer with expertise in JavaScript, HTML,
+        and CSS. Skilled in React, Git, and SASS, I have hands-on experience
+        creating web applications and designing user interfaces. I thrive in
+        collaborative environments, leveraging strong communication and teamwork
+        abilities to contribute effectively. Seeking opportunities to further
+        enhance my skills and make a valuable impact in a stable company
+        committed to innovation.
+      </Typography>
+      <Typography variant="h5">Skills:</Typography>
+      <IconContainer>
+        {skillsData.map(({ icon, title, description }: ISkillsProps) => (
+          <div key={title}>
+            {icon.map((variant) => (
+              <SkillTitle key={variant}>
+                <Icon icon={variant} />
+                <Typography variant="h5">{title}</Typography>
+              </SkillTitle>
+            ))}
+            <Typography>{description}</Typography>
+          </div>
+        ))}
+      </IconContainer>
+    </Root>
+  );
+};
 
 const Root = styled("div")({
   display: "flex",
