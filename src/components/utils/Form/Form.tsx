534581cec2f50b520e19539594292ead60cb3cc1
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2024-11-18 16:41:32 +0200

    Created reusable Form.tsx

---
+++ src/components/utils/Form/Form.tsx
@@ -0,0 +1,42 @@
+import { ReactNode } from "react";
+import styled from "@/DefaultTheme";
+import { Button, Typography } from "@mui/material";
+
+type IFormProps = {
+  name?: string;
+  topic?: string;
+  title?: string;
+  description?: string;
+  href?: string;
+  onSubmit?: () => void;
+  children: ReactNode;
+};
+
+export const Form = ({ children, onSubmit, ...restData }: IFormProps) => (
+  <StyledForm onSubmit={onSubmit}>
+    <Typography variant="h4">Add Project</Typography>
+    <FieldsContainer {...restData}>{children}</FieldsContainer>
+    <Button type="submit" fullWidth title="Add Project">
+      Add Project
+    </Button>
+  </StyledForm>
+);
+
+const StyledForm = styled("form")({
+  display: "flex",
+  flexDirection: "column",
+  justifyContent: "center",
+  alignItems: "center",
+  width: "400px",
+  height: "100%",
+  background: "white",
+  padding: "1rem 2.5rem",
+});
+
+const FieldsContainer = styled("div")({
+  display: "flex",
+  flexDirection: "column",
+  gap: "1rem",
+  width: "100%",
+  margin: "1.5rem 0",
+});
