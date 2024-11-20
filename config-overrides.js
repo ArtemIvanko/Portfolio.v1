9ee1d5421de410c1bbcb3c993fdab8bf5770f238
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2024-11-20 13:13:17 +0200

    Updated custom imports paths declaration, added rule for graphql imports

--- config-overrides.js
+++ config-overrides.js
@@ -1,11 +1,22 @@
+const path = require("path");
 const { alias } = require("react-app-rewire-alias");
 
 module.exports = function override(config) {
+  config.module.rules.push({
+    test: /\.(graphql|gql)$/,
+    exclude: /node_modules/,
+    use: [
+      {
+        loader: "graphql-tag/loader",
+      },
+    ],
+  });
+
   alias({
-    "@": "src",
-    "@shared": "src/components",
-    "@utils": "src/components/utils",
-    "@assets": "src/assets",
+    "@": path.resolve(__dirname, "src"),
+    "@shared": path.resolve(__dirname, "src/components"),
+    "@utils": path.resolve(__dirname, "src/components/utils"),
+    "@assets": path.resolve(__dirname, "src/assets"),
   })(config);
 
   return config;
