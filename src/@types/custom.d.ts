857b1fea81e6e0f38d6b9b506dd2f2909050d505
ArtemIvanko <137142665+ArtemIvanko@users.noreply.github.com>
2024-11-20 14:38:41 +0200

    Moved graphql declaration rule into custom.d.ts. Removed graphql.d.ts

--- src/@types/custom.d.ts
+++ src/@types/custom.d.ts
@@ -24,3 +24,9 @@ declare module "*.pdf" {
   const pdfSrc: string;
   export default pdfSrc;
 }
+
+declare module "*.graphql" {
+  import { DocumentNode } from "graphql";
+  const Schema: DocumentNode;
+  export = Schema;
+}
--- src/@types/graphql.d.ts
+++
@@ -1,7 +0,0 @@
-declare module "*.graphql" {
-  // eslint-disable-next-line @typescript-eslint/ban-ts-comment
-  // @ts-ignore
-  import { DocumentNode } from "graphql"; //TODO Remove ts-ignore
-  const Schema: DocumentNode;
-  export = Schema;
-}
